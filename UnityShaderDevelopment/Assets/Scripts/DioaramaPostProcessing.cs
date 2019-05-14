475d7cc3b00f48af5e4bf252d66e7bd6791e05de
Arturo Nereu <hdgam3r@gmail.com>
2019-05-13 23:45:55 -0500

    Added post processing base scene

---
+++ UnityShaderDevelopment/Assets/Scripts/DioaramaPostProcessing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DioaramaPostProcessing : MonoBehaviour
+{
+    public Material postProcessingMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, postProcessingMaterial);
+    }
+}
